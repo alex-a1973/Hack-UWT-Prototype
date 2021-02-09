25b3d2c656169d4edf2066d6eef081e27e182694
Alex Amado <alex-a1973@Live.com>
2021-02-09 14:26:09 -0800

    Added a components folder with Footer.js and Footer.css component and corresponding .css file, made some changes to App.js including the new Footer component and sick hover effects for the application button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import Footer from './components/Footer';
 import logo from './images/logo1_TB.png';
 import './App.css';
 
+
 const App = () => {
     return (
-        <header className="header-container">
-            <img src={logo} className="logo" alt="logo"/>
-        </header>
+        <React.Fragment>
+            <header className="header-container">
+                <div className="logo-container">
+                    <img src={logo} className="logo" alt="logo"/>
+                </div>
+                <div className="apply-container">
+                    {/* Link to application page */}
+                    <a href="#" className="apply-link">
+                        Apply Now
+                    </a>
+                </div>
+            </header>
+            <Footer/>
+        </React.Fragment>
     );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            
+        </footer>
+    );
+}
+
+export default Footer;
